250318c0b204f175a37de84020e34f3debde1a7d
Andrés Felipe Torres <dev@felipeandres254.io>
2020-12-11 15:43:08 -0500

    Use text node replacement

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,27 @@
-document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, td, caption, span, a')
-    .forEach((element) => {
-        Object.keys(COUNTRY_FLAG_EMOJIS).forEach((unicode) => {
-            if (element.innerHTML.includes(unicode)) {
-                const size = window.getComputedStyle(element, null).getPropertyValue('font-size')
-                const img = `<img style="width: ${size}; height: ${size}; display: inline;" src="${COUNTRY_FLAG_EMOJIS[unicode]}">`
-                element.innerHTML = element.innerHTML.replace(unicode, img)
-            }
-        })
+/**
+ * Gets an array of the text nodes contained by the specified element.
+ * @param  {!Element} element The DOM element which will be traversed.
+ * @return {!Array.<!Node>}
+ */
+const ELEMENT_NODES = [Node.ELEMENT_NODE, Node.DOCUMENT_NODE, Node.DOCUMENT_TYPE_NODE]
+function getTextNodes(element) {
+    if (!element)
+        return []
+    return Array.from(element.childNodes).reduce((nodes, node) => {
+        if (node.nodeType == Node.TEXT_NODE)
+            nodes.push(node)
+        else if (ELEMENT_NODES.includes(node.nodeType))
+            nodes = nodes.concat(getTextNodes(node))
+        return nodes
+    }, [])
+}
+
+Object.keys(COUNTRY_FLAG_EMOJIS).forEach((unicode) => {
+    getTextNodes(document.querySelector('body')).forEach((node) => {
+        if (node.data.includes(unicode)) {
+            const size = window.getComputedStyle(node.parentElement, null).getPropertyValue('font-size')
+            const img = `<img style="width: ${size}; height: ${size}; display: inline;" src="${COUNTRY_FLAG_EMOJIS[unicode]}">`
+            node.parentElement.innerHTML = node.parentElement.innerHTML.replace(new RegExp(unicode, 'g'), img)
+        }
     })
+})
